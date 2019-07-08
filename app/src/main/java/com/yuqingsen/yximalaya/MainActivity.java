8d3675df66eb5cc0167f2782989c788e65445065
木木木199306 <31744153@qq.com>
2019-07-08 17:54:05 +0800

    集成了喜马拉雅SDK,并且测试可以获取到数据

--- app/src/main/java/com/yuqingsen/yximalaya/MainActivity.java
+++ app/src/main/java/com/yuqingsen/yximalaya/MainActivity.java
@@ -1,13 +1,45 @@
 package com.yuqingsen.yximalaya;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Map<String, String> map = new HashMap<>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(@Nullable CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+                if (categories!=null){
+                    int size = categories.size();
+                    Log.d(TAG,"categories size-------<"+size);
+                    for (Category category:categories){
+                        Log.d(TAG, "categorie----->"+category.getCategoryName());
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.e(TAG, "onError: "+i+"errorMsg == >"+s);
+            }
+        });
     }
 }

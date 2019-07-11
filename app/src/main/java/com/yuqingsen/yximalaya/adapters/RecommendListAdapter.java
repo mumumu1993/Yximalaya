f8aff6475684604d218b165375d225feee2495d3
木木木199306 <31744153@qq.com>
2019-07-12 03:58:35 +0800

    推荐页数据获取与展示

---
+++ app/src/main/java/com/yuqingsen/yximalaya/adapters/RecommendListAdapter.java
@@ -0,0 +1,78 @@
+package com.yuqingsen.yximalaya.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+import com.yuqingsen.yximalaya.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecommendListAdapter extends RecyclerView.Adapter<RecommendListAdapter.InnerHolder> {
+
+    private List<Album> data = new ArrayList<>();
+    @NonNull
+    @Override
+    public InnerHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_recommend,viewGroup,false);
+        return new InnerHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InnerHolder innerHolder, int i) {
+        innerHolder.itemView.setTag(i);
+        innerHolder.setData(data.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        if (data!=null){
+            return data.size();
+        }
+        return 0;
+    }
+
+    public void setData(List<Album> albumList) {
+        if (data!=null){
+            data.clear();
+            data.addAll(albumList);
+        }
+        //更新UI
+        notifyDataSetChanged();
+    }
+
+    public class InnerHolder extends RecyclerView.ViewHolder {
+
+        public InnerHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void setData(Album album) {
+            //找到控件，设置数据
+            //专辑封面
+            ImageView albumCoverIv = itemView.findViewById(R.id.album_cover);
+            //标题
+            TextView albumTitleTv = itemView.findViewById(R.id.album_title_tv);
+            //描述
+            TextView albumDesTv = itemView.findViewById(R.id.album_description_tv);
+            //播放数量
+            TextView albumPlayCount = itemView.findViewById(R.id.album_play_count);
+            //专辑内容数量
+            TextView albumContentSize = itemView.findViewById(R.id.album_content_size);
+
+            albumTitleTv.setText(album.getAlbumTitle());
+            albumDesTv.setText(album.getAlbumIntro());
+            albumPlayCount.setText(album.getPlayCount()+"");
+            albumContentSize.setText(album.getIncludeTrackCount()+"");
+
+            Picasso.with(itemView.getContext()).load(album.getCoverUrlLarge()).into(albumCoverIv);
+        }
+    }
+}
--- app/src/main/java/com/yuqingsen/yximalaya/fragments/RecommendFragment.java
+++ app/src/main/java/com/yuqingsen/yximalaya/fragments/RecommendFragment.java
@@ -1,16 +1,85 @@
 package com.yuqingsen.yximalaya.fragments;
 
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+import com.ximalaya.ting.android.opensdk.model.album.GussLikeAlbumList;
 import com.yuqingsen.yximalaya.R;
+import com.yuqingsen.yximalaya.adapters.RecommendListAdapter;
 import com.yuqingsen.yximalaya.base.BaseFragment;
+import com.yuqingsen.yximalaya.utils.Constants;
+import com.yuqingsen.yximalaya.utils.LogUtil;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RecommendFragment extends BaseFragment {
+    private static final String TAG = "RecommendFragment";
+    private View rootView;
+    private RecyclerView recommendList;
+    private RecommendListAdapter recommendListAdapter;
     @Override
     protected View onSubViewLoaded(LayoutInflater layoutInflater, ViewGroup container) {
-        View rootView = layoutInflater.inflate(R.layout.fragment_recommend,container,false);
+        rootView = layoutInflater.inflate(R.layout.fragment_recommend,container,false);
+        recommendList = rootView.findViewById(R.id.recommend_list);
+        //设置布局管理器
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recommendList.setLayoutManager(linearLayoutManager);
+        //设置适配器
+        recommendListAdapter = new RecommendListAdapter();
+        recommendList.setAdapter(recommendListAdapter);
+
+        //拿数据回来
+        getRecommendData();
+
+
+        //返回view给界面显示
         return rootView;
+    }
+
+    /**
+     * 获取推荐内容
+     * 实现接口：
+     * 3.10.6 获取猜你喜欢专辑
+     */
+    private void getRecommendData() {
+        //封装参数
+        Map<String, String> map = new HashMap<>();
+        //一页数据返回条数
+        map.put(DTransferConstants.LIKE_COUNT, Constants.RECOMMEND_COUNT+"");
+
+        CommonRequest.getGuessLikeAlbum(map, new IDataCallBack<GussLikeAlbumList>() {
+            @Override
+            public void onSuccess(@Nullable GussLikeAlbumList gussLikeAlbumList) {
+                //数据获取成功
+                if (gussLikeAlbumList!=null){
+                    List<Album> albumList = gussLikeAlbumList.getAlbumList();
+                    //拿到数据，更新UI
+                    upRecommendUI(albumList);
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                //获取数据失败
+                LogUtil.d(TAG,"error -- >"+i);
+                LogUtil.d(TAG,"errorMsg -- >"+s);
+            }
+        });
+    }
+
+    private void upRecommendUI(List<Album> albumList) {
+        //把数据设置给适配器并更新
+        recommendListAdapter.setData(albumList);
     }
 }
